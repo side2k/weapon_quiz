bbc4900388ef6b776175a30b10a1d2d44a6d9b4b
Leonid Amirov <leonid.amirov@gmail.com>
2015-01-12 20:32:09 +0300

    importing quiz from textfile

--- weapon_quiz/management/commands/import_questions.py
+++ weapon_quiz/management/commands/import_questions.py
@@ -1,14 +1,36 @@
 import re
+
 from django.core.management.base import BaseCommand, CommandError
 
+from multichoice.models import MCQuestion, Answer
+from quiz.models import Quiz
+
 class Command(BaseCommand):
-    args = '<quiz_id> <data_file>'
+    args = '<data_file>'
     help = 'Imports questions from text file'
 
-    def handle(self, quiz_id, filename, *args, **options):
+    def handle(self, filename, *args, **options):
+        verbosity = int(options.get('verbosity', '1'))
+        
         data_file = open(filename, 'r')
         state = None
         question_count = 0
+        quiz = None
+        quiz_index = 0
+        while not quiz:
+            quiz_url = "quiz%d" % quiz_index
+            if not Quiz.objects.filter(url=quiz_url):
+                quiz = Quiz()
+            else:
+                quiz_index += 1
+                continue
+            quiz.title = 'Imported quiz #%d' % quiz_index
+            quiz.url = quiz_url
+            quiz.save()
+
+        answers = {}
+        question = None
+
         for line in data_file:
             match = re.match(r'^ *(?P<answer>\d+) *\.? *$', line)
             if match:
@@ -20,19 +42,35 @@ class Command(BaseCommand):
 
             if not state:
                 #question start
-                self.stdout.write(u'Question #%s: %s' % (num, text))
+                answers = {}
+                question = MCQuestion()
+                question.content = text
+                question.save()
+                quiz.question_set.add(question)
+                if verbosity >= 2:
+                    self.stdout.write(u'Question #%s: %s' % (num, text))
                 state = 'variant'
 
             elif state == 'variant':
-                self.stdout.write(u'Variant #%s: %s' % (num, text))
+                variant = Answer()
+                variant.content = text
+                variant.question = question
+                variant.save()
+                answers[num] = variant
+                if verbosity >= 2:
+                    self.stdout.write(u'Variant #%s: %s' % (num, text))
 
             elif state == 'answer':
-                self.stdout.write('Right answer: %s' % match.group('answer'))
+                answer = match.group('answer')
+                variant = answers[answer]
+                variant.correct = True
+                variant.save()
+                if verbosity >= 2:
+                    self.stdout.write('Right answer: %s' % answer)
                 question_count += 1
                 state = None
 
-            if question_count >= 3:
-                break
         data_file.close()
 
         self.stdout.write('Imported %d questions' % question_count)
+        self.stdout.write('Quiz title: %s' % quiz.title)
